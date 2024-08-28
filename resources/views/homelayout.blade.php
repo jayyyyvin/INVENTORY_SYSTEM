ca232c76df34479852d35aeeaeedb2b93038b1af
jayyyyvin <130818495+jayyyyvin@users.noreply.github.com>
2024-08-28 22:49:02 +0800

    api namo

--- resources/views/homelayout.blade.php
+++ resources/views/homelayout.blade.php
@@ -107,11 +107,20 @@
                     <i class="fa-solid fa-truck-field"></i>
                     <span>Suppliers</span></a>
             </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="/orders">
+                    <i class="fa-solid fa-truck-field"></i>
+                    <span>Orders</span></a>
+            </li>
+
             <li class="nav-item ">
                 <a class="nav-link" href="{{route('warehouse.index')}}">
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>WareHouse</span></a>
             </li>
+
+
 
             <!-- Nav Item - Pages Collapse Users -->
             <li class="nav-item">
